3fbe0b0e96c6456e682268964171a88bae563fbf
Yehor Kadeniuk <91326774+newmasterSG@users.noreply.github.com>
2024-03-16 08:18:37 +0200

    add

--- Test2/Program.cs
+++ Test2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("fwef");
+            double num1, num2;
+            char operation;
+
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            operation = Convert.ToChar(Console.ReadLine());
+
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"Result: {result}");
+            Console.WriteLine("---------------------------");
         }
     }
 }
